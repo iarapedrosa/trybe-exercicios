094b7389055662f9480ffd9475950e90503786ca
Iara Pedrosa <iaraferreirahg@gmail.com>
2021-11-10 16:01:22 -0300

    atualizando com os novos exercicios

---
+++ fundamentos/bloco-5-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,7 @@
+const myName = "Iara";
+const birthCity = "Sao Luis";
+let birthYear = 1998;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Caruaru"
